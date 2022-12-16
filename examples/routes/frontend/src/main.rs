31878dd7f8b045910a17e34d71abd29c2e14e91a
arn-the-long-beard <arn.menant@gmail.com>
2022-12-16 15:59:44 +0100

    feature: updated the button creation and now selected state works and the query is built ( need to be improved ) and displayed in the console

--- examples/routes/frontend/src/main.rs
+++ examples/routes/frontend/src/main.rs
@@ -1,35 +1,54 @@
-use zoon::*;
+use zoon::eprintln;
 use zoon::named_color::{BLUE_0, BLUE_8, GREEN_0, GREEN_1, GREEN_5, GREEN_8};
+use zoon::routing::url;
+use zoon::Text;
+use zoon::*;
 
-
-fn root() -> impl Element {
-    Column::new().items(vec![
-   Row::new().item("Select your training today"),
-   Row::new()
-       .item(Column::new() .items(vec![
-           Row::new().items(vec![cell("Leg day"),cell("Chest day")]),
-           Row::new().items(vec![cell("Let's do pull up and shoulders"),cell("I am resting")])
-       ])
-    )])
+#[static_ref]
+fn selection() -> &'static Mutable<String> {
+    Mutable::new(String::new())
 }
 
-fn cell(name : &str) -> impl Element {
-    let selected= Mutable::new(false);
-        Button::new()
-            .s(Width::exact(150)).s(Height::exact(50))
+fn root() -> impl Element {
+    Column::new()
+        .items(vec![
+            Row::new().item("Select your training today"),
+            Row::new().item(Column::new().items(vec![
+                Row::new().items(vec![update_button("Leg day"), update_button("Chest day")]),
+                Row::new().items(vec![
+                    update_button("Let's do pull up and shoulders"),
+                    update_button("I am resting"),
+                ]),
+            ])),
+        ])
+        .item(Row::new().item(Text::with_signal(selection().signal())))
+}
+
+fn update_button(name: &str) -> impl Element {
+    let selected = Mutable::new(false);
+    let query = format!("selected={}", name);
+    let task = Task::start_droppable(selected.signal().for_each_sync(move |sel| {
+        eprintln!("is selected ? {}", sel);
+        if sel {
+            selection().set(selection().take() + &query.to_string());
+        } else {
+            // Todo find the string and remove it problably
+        }
+        eprintln!(" Query is {:?}", &selection())
+    }));
+
+    Button::new()
+        .s(Width::exact(150))
+        .s(Height::exact(50))
         .s(Borders::all_signal(selected.signal().map_bool(
             || Border::new().color(BLUE_8).dashed(),
             || Border::new().color(BLUE_0).dotted(),
         )))
-            .s(Background::new().color_signal(selected.signal().map_bool(
-                || GREEN_5,
-                || GREEN_8
-            )))
-        .on_press(move || selected.set_neq(!selected.take()))
+        .s(Background::new().color_signal(selected.signal().map_bool(|| GREEN_5, || GREEN_8)))
+        .on_press(move || selected.update(|select| !select))
+        .after_remove(move |_| drop(task))
         .label(name)
 }
-
-
 
 // ---------- // -----------
 
